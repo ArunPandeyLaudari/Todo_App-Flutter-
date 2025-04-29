eaf0f2068d02b0ddf22f3e5c3a7d130bd5f35f9c
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-04-29 10:24:04 +0545

    ui updated

---
+++ lib/model/todo.dart
@@ -0,0 +1,14 @@
+class Todo{
+  final String id;
+  final String title;
+  final String description;
+  final bool isCompleted;
+
+Todo({
+  required this.id,
+  required this.title,
+  required this.description,
+  this.isCompleted=false
+});
+
+}
